a1877159a9401c4b09ef841d71ed714dcea16603
John Hinch <123460465+KnightwingJ@users.noreply.github.com>
2023-12-13 15:38:41 +0000

    Update worker.py

--- worker.py
+++ worker.py
@@ -1,34 +1,40 @@
 from flask import Flask
 from flask import request
+from flask import render_template
+from google.cloud import secretmanager
 import requests
 import os
 import json
 app = Flask(__name__)
 
-def get_api_key() -> str:
-    secret = os.environ.get("COMPUTE_API_KEY")
-    if secret:
-        return secret
-    else:
-        #local testing
-        with open('.key') as f:
-            return f.read()
+def access_secret_version(secret_id, version_id="latest"):
+    # Create the Secret Manager client.
+    client = secretmanager.SecretManagerServiceClient()
+
+    # Build the resource name of the secret version.
+    name = f"projects/533118755108/secrets/{secret_id}/versions/{version_id}"
+
+    # Access the secret version.
+    response = client.access_secret_version(name=name)
+
+    # Return the decoded payload.
+    return response.payload.data.decode('UTF-8')
       
 @app.route("/")
 def hello():
-    return "Add workers to the Spark cluster with a POST request to add   "
+    return "Add workers to the Spark cluster with a POST request to add"
 
 @app.route("/test")
 def test():
     #return "Test" # testing 
-    return(get_api_key())
+    return(access_secret_version("compute-api-key"))
 
 @app.route("/add",methods=['GET','POST'])
 def add():
   if request.method=='GET':
-    return "Use post to add" # replace with form template
+    return "Use post method"
   else:
-    token=get_api_key()
+    token=access_secret_version("compute-api-key")
     ret = addWorker(token,request.form['num'])
     return ret
 
@@ -46,6 +52,11 @@ def addWorker(token, num):
     else:
       print(resp.content)
       return "Error\n"+resp.content.decode('utf-8') + '\n\n\n'+data
+
+
+
+if __name__ == "__main__":
+    app.run(host='0.0.0.0',port='8080')
 
 
 
